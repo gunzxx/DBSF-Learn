58325641864c59d98110bbf4402c12932a7679e2
gunzxx <ggm5850@gmail.com>
2023-06-26 20:02:56 +0700

    export import module

---
+++ 01-Javascript/Materi/07 Module/02 Export beberapa nilai/index.js
@@ -0,0 +1,12 @@
+const {coffeeStock,isCoffeeMachineReady} = require('./state');
+console.log(coffeeStock);
+
+const makeCoffee = (type, miligrams) => {
+    if (coffeeStock[type] >= miligrams) {
+        console.log("Kopi berhasil dibuat!");
+    } else {
+        console.log("Biji kopi habis!");
+    }
+}
+
+makeCoffee("robusta", 80);
---
+++ 01-Javascript/Materi/07 Module/02 Export beberapa nilai/state.js
@@ -0,0 +1,9 @@
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    liberica: 200
+}
+
+const isCoffeeMachineReady = true
+module.exports = {coffeeStock,isCoffeeMachineReady};
+// console.log(module);
